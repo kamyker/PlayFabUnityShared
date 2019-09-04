2ece165a46fa6a49bb7f93c2b748c4dbe86409d9
kamyker <kamyker@o2.pl>
2019-09-04 20:39:03 +0200

    no message

--- Source/PlayFabSharedSettings.cs
+++ Source/PlayFabSharedSettings.cs
@@ -5,54 +5,63 @@ using UnityEngine;
 [assembly: InternalsVisibleTo("PlayFabSDK")]
 namespace PlayFab
 {
-	[CreateAssetMenu(fileName = "PlayFabSharedSettings", menuName = "PlayFab/CreateSharedSettings", order = 1)]
-	public class PlayFabSharedSettings : ScriptableObject
-	{
-		public string TitleId;
-		internal string VerticalName = null;
+    [CreateAssetMenu(fileName = "PlayFabSharedSettings", menuName = "PlayFab/CreateSharedSettings", order = 1)]
+    public class PlayFabSharedSettings : ScriptableObject
+    {
+        public string TitleId;
+        internal string VerticalName = null;
 #if ENABLE_PLAYFABSERVER_API || ENABLE_PLAYFABADMIN_API || UNITY_EDITOR
-		public string DeveloperSecretKey;
+        public string DeveloperSecretKey;
 #endif
-		public string ProductionEnvironmentUrl = "";
+        public string ProductionEnvironmentUrl = "";
 
-		public WebRequestType RequestType = WebRequestType.UnityWebRequest;
+        public WebRequestType RequestType = WebRequestType.UnityWebRequest;
 
-		public string AdvertisingIdType;
-		public string AdvertisingIdValue;
+        public string AdvertisingIdType;
+        public string AdvertisingIdValue;
 
-		public bool DisableAdvertising;
-		public bool DisableDeviceInfo;
-		public bool DisableFocusTimeCollection;
+        public bool DisableAdvertising;
+        public bool DisableDeviceInfo;
+        public bool DisableFocusTimeCollection;
 
-		public int RequestTimeout = 2000;
-		public bool RequestKeepAlive = true;
-		public bool CompressApiData = true;
+        public int RequestTimeout = 2000;
+        public bool RequestKeepAlive = true;
+        public bool CompressApiData = true;
 
-		public PlayFabLogLevel LogLevel = PlayFabLogLevel.Warning | PlayFabLogLevel.Error;
-		public string LoggerHost = "";
-		public int LoggerPort = 0;
-		public bool EnableRealTimeLogging = false;
-		public int LogCapLimit = 30;
-	}
+        public PlayFabLogLevel LogLevel = PlayFabLogLevel.Warning | PlayFabLogLevel.Error;
+        public string LoggerHost = "";
+        public int LoggerPort = 0;
+        public bool EnableRealTimeLogging = false;
+        public int LogCapLimit = 30;
 
-	public enum WebRequestType
-	{
+        public static PlayFabSharedSettings LoadFromResources()
+        {
+            var settingsList = Resources.LoadAll<PlayFabSharedSettings>("PlayFabSdk");
+            if (settingsList.Length != 1)
+                throw new System.Exception("The number of PlayFabSharedSettings objects should be 1: " + settingsList.Length);
+            return settingsList[0];
+        }
+    }
+
+
+    public enum WebRequestType
+    {
 #if !UNITY_2018_2_OR_NEWER // Unity has deprecated Www
         UnityWww, // High compatability Unity api calls
 #endif
-		UnityWebRequest, // Modern unity HTTP component
-		HttpWebRequest, // High performance multi-threaded api calls
-		CustomHttp //If this is used, you must set the Http to an IPlayFabHttp object.
-	}
+        UnityWebRequest, // Modern unity HTTP component
+        HttpWebRequest, // High performance multi-threaded api calls
+        CustomHttp //If this is used, you must set the Http to an IPlayFabHttp object.
+    }
 
-	[Flags]
-	public enum PlayFabLogLevel
-	{
-		None = 0,
-		Debug = 1 << 0,
-		Info = 1 << 1,
-		Warning = 1 << 2,
-		Error = 1 << 3,
-		All = Debug | Info | Warning | Error,
-	}
+    [Flags]
+    public enum PlayFabLogLevel
+    {
+        None = 0,
+        Debug = 1 << 0,
+        Info = 1 << 1,
+        Warning = 1 << 2,
+        Error = 1 << 3,
+        All = Debug | Info | Warning | Error,
+    }
 }

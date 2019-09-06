7aab9803d3b7ff1ab4ce813a853eb0d19aed5a1a
kamyker <kamyker@o2.pl>
2019-09-06 05:42:12 +0200

    ver up, added easiest way to fire-forget task api calls

---
+++ Source/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PlayFab
+{
+    public static class Extensions
+    {
+        public static async void FireForgetLog(this Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
